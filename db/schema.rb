13497c0bb1fc6f97fed584f67bc31b1fb6ec9a59
keikamiguchi <kei.kamiguchi517@gmail.com>
2022-11-30 22:10:10 +0900

    fix: カラム名変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_132812) do
+ActiveRecord::Schema.define(version: 2022_11_30_130654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2020_11_28_132812) do
   create_table "purchase_histories", force: :cascade do |t|
     t.integer "product_id"
     t.integer "quantity"
-    t.integer "total_mount"
+    t.integer "total_amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
